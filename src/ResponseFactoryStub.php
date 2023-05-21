9531444c9fca8232288f1231e25801ddcda19c76
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-05-21 02:01:30 -0500

    feat(ResponseFactoryStub): all for adding streams to mocked responses

--- src/ResponseFactoryStub.php
+++ src/ResponseFactoryStub.php
@@ -6,11 +6,17 @@ namespace Phpolar\HttpMessageTestUtils;
 
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 final class ResponseFactoryStub implements ResponseFactoryInterface
 {
+    public function __construct(private ?StreamInterface $stream = null)
+    {
+    }
+
     public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
     {
-        return new ResponseStub($code, $reasonPhrase);
+        $response = new ResponseStub($code, $reasonPhrase);
+        return $this->stream === null ?  $response : $response->withBody($this->stream);
     }
 }
